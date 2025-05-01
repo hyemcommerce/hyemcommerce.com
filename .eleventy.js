d2caa748fa972320221ad224b2c9eb9a4ac2e401
Daryl Shaw <daryl@darylshaw.co.uk>
2025-05-01 15:53:43 +0100

    Uploaded logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("_redirects");
+    eleventyConfig.addPassthroughCopy("img");
   };
